09c942963fbd03c76f9dd4fa2488271d3f2cafef
Osher Kramer <osherkramer@gmail.com>
2015-09-08 21:54:35 +0300

    add comment

--- CLI/src/Run.java
+++ CLI/src/Run.java
@@ -2,7 +2,7 @@ import controller.Controller;
 import controller.MyController;
 
 public class Run {
-
+	//
 	public static void main(String[] args) {
 		Controller controller = new MyController();
 		
